3d92252840159c0113ce3769900fd9fd8fc91e63
BearWithMe18 <77765510+BearWithMe18@users.noreply.github.com>
2025-05-15 17:15:05 -0400

    Added Contact Link to Header

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const header = `<nav class="nav border-bottom bg-body-tertiary">
         <a class="nav-link" aria-current="page" href="index.html" style="color: aliceblue; font-weight: 500;">Home</a>
         <a class="nav-link" href="resume.html">Resume</a>
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link" href="contact.html">Contact</a>
         <a class="nav-link" href="https://github.com/BearWithMe18">Github</a>
         <a class="nav-link" href="https://www.linkedin.com/in/noah-leach-79650116b">LinkedIn</a>
     </nav>`
